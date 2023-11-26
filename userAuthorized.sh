fc82ca3c33016cd5bbc9499a68d810362703ad4d
Sophia Liu <75860082+soaphia14@users.noreply.github.com>
2023-11-26 17:18:21 -0500

    Update userAuthorized.sh

--- userAuthorized.sh
+++ userAuthorized.sh
@@ -3,14 +3,14 @@
 # NOTHING ABOUT ADDING OR ADMINS!!
 AUTHORIZED_USERS="BUFFER_BEGINNING
 
-BUFFER_END" # separate with "/"
+BUFFER_END" # separate with "/", REMEMBER TO ADD ADMINS!
 
 IFS='/' read -r -a AUTHORIZED_USERS_ARRAY <<< "$AUTHORIZED_USERS"
 mapfile -t CURRENT_USERS  < <(ls /home)
 
 for user in "${CURRENT_USERS[@]}"; do
   echo "USER: $user"
-  if [[ " ${AUTHORIZED_USERS_ARRAY[*]} " =~ " ${item} " ]]; then
+  if [[ " ${AUTHORIZED_USERS_ARRAY[*]} " =~ " ${user} " ]]; then
     echo "YES - should be here"
   else
     echo "NO - shouldn't be here (check, then delete)"
