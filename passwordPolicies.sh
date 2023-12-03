24e55791beeb4336a54ff3a173dd14aff75239b2
Sophia Liu <75860082+soaphia14@users.noreply.github.com>
2023-12-03 11:45:09 -0500

    Update passwordPolicies.sh

--- passwordPolicies.sh
+++ passwordPolicies.sh
@@ -5,7 +5,7 @@ users=$(awk -F: '{ print $1}' /etc/passwd)
 for value in $users
 do 
       echo $value
-      chage -M 60 -W 7 $value # max days 60, min days 5, warn age 7
+      chage -M 60 -m 5 -W 7 $value # max days 60, min days 5, warn age 7
       chage -l $value
       echo "....."
       echo ""
